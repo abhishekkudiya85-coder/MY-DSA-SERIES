f18e43d5f9a5487da517c73ce2864a4e15278360
Abhishek kudiya <abhishekkudiya85@gmail.com>
2026-06-11 13:26:26 +0530

    Add Array Initialization and Access program

---
+++ Arrays/Array-Initialization-and-Access.cpp
@@ -0,0 +1,26 @@
+/*
+Program: Array Initialization and Access
+Language: C++
+Time Complexity: O(1)
+Space Complexity: O(1)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double marks[5] = {10, 20, 30, 40};
+
+    cout << "\nmarks = " << marks[0];
+    cout << "\nmarks = " << marks[1];
+    cout << "\nmarks = " << marks[2];
+    cout << "\nmarks = " << marks[3];
+
+    return 0;
+}
+// Output
+// marks = 10
+// marks = 20
+// marks = 30
+// marks = 40
